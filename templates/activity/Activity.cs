597ca8368dbf2ffcf20645a5fb17b5a8b589ddfc
Andres Ravelo <ravelaso@proton.me>
2025-09-23 13:54:35 +0200

    add examples separation for activity project

---
+++ templates/activity/Activity.cs
@@ -0,0 +1,18 @@
+using System.Activities;
+using System.ComponentModel;
+using System.Diagnostics;
+using ProjectName.Helpers;
+using UiPath.Robot.Activities.Api;
+
+namespace ProjectName;
+
+public class Activity : CodeActivity
+{
+    // Define argument properties
+
+    protected override void Execute(CodeActivityContext context)
+    {
+        // Do something
+    }
+}
+
